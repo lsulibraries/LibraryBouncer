d1ff6f5d332486eed987c9881798065cf1c50fbb
GarrettArm <garrett.w.armstrong@gmail.com>
2019-11-11 16:11:54 -0600

    some amount of logging

--- BouncerAPI/app.py
+++ BouncerAPI/app.py
@@ -1,9 +1,24 @@
 #! usr/env/python3
 
+import logging
+from datetime import datetime
+
 from flask import Flask, json, request, render_template, jsonify
 import requests
 
 application = Flask(__name__)
+
+logging.basicConfig(
+    filename="access_stats.txt",
+    level=logging.INFO,
+    format="%(asctime)s ----- %(message)s",
+)
+
+
+def parse_secrets():
+    with open("user_pass.json", "r") as f:
+        info = json.load(f)
+    return info["user"], info["password"]
 
 
 def login(username, password):
@@ -11,6 +26,16 @@ def login(username, password):
     s = requests.session()
     r = s.post(endpoint)
     return s, json.loads(r.text)["sessionToken"]
+
+
+def get_userinfo(session, session_token, userid):
+    endpoint = pick_an_endpoint(userid, session_token)
+    if not endpoint:
+        return {"user": "Unknown user", "expiration": "1900-01-01"}
+    response = session.post(endpoint)
+    parsed = parse_response(response)
+    log_access(parsed)
+    return parsed
 
 
 def pick_an_endpoint(userid, session_token):
@@ -21,7 +46,7 @@ def pick_an_endpoint(userid, session_token):
     elif len(userid) == 9 and userid[:2] == "89":
         endpoint = f"https://lalu.sirsi.net/lalu_ilsws/rest/patron/lookupPatronInfo?clientID=DS_CLIENT&sessionToken={session_token}&alternateID={userid}&includePatronStatusInfo=True&includePatronInfo=True&json=True"
     else:
-        # userid isn't an 89 number or a 17-digit account number
+        # if userid isn't an 89 number or a 17-digit account number
         endpoint = None
     return endpoint
 
@@ -38,19 +63,13 @@ def parse_response(r):
     return {"user": user, "expiration": expiration}
 
 
-def get_userinfo(session, session_token, userid):
-    endpoint = pick_an_endpoint(userid, session_token)
-    if not endpoint:
-        return {"user": "Unknown user", "expiration": "1900-01-01"}
-    print(endpoint)
-    response = session.post(endpoint)
-    return parse_response(response)
-
-
-def parse_secrets():
-    with open("user_pass.json", "r") as f:
-        info = json.load(f)
-    return info["user"], info["password"]
+def log_access(parsed):
+    user_exp = datetime.strptime(parsed["expiration"], "%Y-%m-%d")
+    now = datetime.now()
+    if now > user_exp:
+        logging.info("Denied")
+    else:
+        logging.info("Allowed")
 
 
 @application.route("/")
@@ -60,6 +79,17 @@ def index():
     userid = request.args.get("id")
     userinfo = get_userinfo(session, session_token, userid)
     return jsonify(userinfo)
+
+
+@application.route("/stats/")
+def stats():
+    with open("access_stats.txt", "r") as f:
+        lines = [line for line in f.read().split('\n') if line]
+    stats = []
+    for line in lines:
+        t, s = line.split(" ----- ")
+        stats.append({"time": t, "success": s})
+    return jsonify(stats)
 
 
 if __name__ == "__main__":
